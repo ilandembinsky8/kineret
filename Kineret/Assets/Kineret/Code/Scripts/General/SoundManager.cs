b4b8cc5597082e59b82b81c66e0ef02e390894e1
Sharon Vishnivetsky <svds912@gmail.com>
2026-03-04 22:24:54 +0200

    Added Background music in flight

---
+++ Kineret/Assets/Kineret/Code/Scripts/General/SoundManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+}
