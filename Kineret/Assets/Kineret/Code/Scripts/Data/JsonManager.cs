43283c0a53623b5f44fe0166c10de9894b1ee4cc
Sharon Vishnivetsky <svds912@gmail.com>
2026-03-13 01:10:49 +0200

    Json Changes to reformatted SO structure

--- Kineret/Assets/Kineret/Code/Scripts/Data/JsonManager.cs
+++ Kineret/Assets/Kineret/Code/Scripts/Data/JsonManager.cs
@@ -7,13 +7,53 @@ public class JsonManager : MonoBehaviour
 {
     public GameData Data;
 
+    #region Serialized fields
     [SerializeField] CollectableData BonusPointRange;
     [SerializeField] CollectableData DestinationRange;
     [SerializeField] CollectableData InterestPointRange;
 
+    #region Destinations
+    [Header("Destinations")]
+    [SerializeField] private DestinationSO Afimilk;
+    [SerializeField] private DestinationSO AgmonHula;
+    [SerializeField] private DestinationSO Agre;
+    [SerializeField] private DestinationSO BioCastle;
+    [SerializeField] private DestinationSO Eshkol;
+    [SerializeField] private DestinationSO Gilboa;
+    [SerializeField] private DestinationSO Ginosar;
+    [SerializeField] private DestinationSO Golan;
+    [SerializeField] private DestinationSO Salmon;
+    [SerializeField] private DestinationSO Shamir;
+    [SerializeField] private DestinationSO Tzemah;
+    #endregion
+
+    [Header("Interest Points")]
+    [SerializeField] private InterestPointSO Biriya;
+    [SerializeField] private InterestPointSO Keshet;
+    [SerializeField] private InterestPointSO SwitzForest;
+    [SerializeField] private InterestPointSO Tzipori;
+    #endregion
+
     private bool _dataIsLoaded;
 
     private void Awake() { _dataIsLoaded = LoadFromJson(); }
+    private void Start()
+    {
+        if (_dataIsLoaded)
+        {
+            //Afimilk.DestinationData = Data.Afimilk;
+            //AgmonHula.DestinationData = Data.AgmonHula;
+            //Agre.DestinationData = Data.Agre;
+            //BioCastle.DestinationData = Data.BioCastle;
+            //Eshkol.DestinationData = Data.Eshkol;
+            //Gilboa.DestinationData = Data.Gilboa;
+            //Ginosar.DestinationData = Data.Ginosar;
+            //Golan.DestinationData = Data.Golan;
+            //Salmon.DestinationData = Data.Salmon;
+            Shamir.DestinationData = Data.Shamir;
+            Tzemah.DestinationData = Data.Tzemah;
+        }
+    }
 
     [ContextMenu("Load From JSON")]
     public bool LoadFromJson()
@@ -36,70 +76,55 @@ public class JsonManager : MonoBehaviour
 
         Data = JsonUtility.FromJson<GameData>(json);
 
-        #region validation checks
-        if (Data == null)
-        {
-            Debug.LogWarning($"{nameof(JsonManager)}: JSON is missing/empty.");
-            return false;
-        }
-
-        if (Data.DestinationsPoolData == null)
-        {
-            Debug.LogWarning("DestinationsPoolData missing.");
-            return false;
-        }
-
-        if (Data.ChallengesPoolData == null)
-        {
-            Debug.LogWarning("ChallengesPoolData missing.");
-            return false;
-        }
-
-        if (Data.BonusPoolData == null)
-        {
-            Debug.LogWarning("BonusPoolData missing.");
-            return false;
-        }
-
-        if (Data.StaticInterestPoolData == null)
-        {
-            Debug.LogWarning("StaticInterestPoolData missing.");
-            return false;
-        }
-
-        /*foreach (var destination in Data.DestinationsPoolData)
-        {
-            if (destination.DidYouKnowPoints == null || destination.DidYouKnowPoints.Count != 3)
-            {
-                Debug.LogWarning("Destination doesn't contain exactly 3 DidYouKnowPoints.");
-                return false;
-            }
-        }*/
-        #endregion
-
         Debug.Log($"{nameof(JsonManager)}: JSON loaded successfully.");
         return true;
     }
 
 }
 
+/////////////////////////////////////////////////////////////////////////////////////
+///////////////////////         V Structs V          ////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
 [Serializable]
-public class GameData
+public struct GameData
 {
-    //Tomer: I believe not using a list and having a field for each location would:
-    //A. Remove the need of the ID in the json, as now I can match them to the enum by field.
-    //B. Make it obvious which data goes where as that would be the name of the field and not a random spot in the collection
-    //For example the field would be named TzemahData, can't imagine someone not realizing what data to put there.
-    public List<DestinationData> DestinationsPoolData;
-    public List<ChallengePoints> ChallengesPoolData;
-    public List<CollectablePoints> BonusPoolData;
-    public List<StaticInterestPoints> StaticInterestPoolData;
+    public DestinationData Afimilk;
+    public DestinationData AgmonHula;
+    public DestinationData Agre;
+    public DestinationData BioCastle;
+    public DestinationData Eshkol;
+    public DestinationData Gilboa;
+    public DestinationData Ginosar;
+    public DestinationData Golan;
+    public DestinationData Salmon;
+    public DestinationData Shamir;
+    public DestinationData Tzemah;
+
+    public InfoCollectableData Biriya;
+    public InfoCollectableData Keshet;
+    public InfoCollectableData SwitzForest;
+    public InfoCollectableData Tzipori;
+
+    public InfoCollectableData HayadataCollecableData;
+
+    public BonusCollectableData CarbonBonusData;
+    public BonusCollectableData EnergyBonusData;
+    public BonusCollectableData WaterBonusData;
+    public BonusCollectableData SoilBonusData;
+
+    public ChallengePointData FrontWindChallengeData;
+    public ChallengePointData SideWindChallengeData;
+    public ChallengePointData BirdChallengeData;
 }
 
+#region Destination Data
+/// <summary>
+/// Main data structure of a destination
+/// </summary>
 [Serializable]
 public struct DestinationData
 {
-    public int Id;
     public string Name;
     public string Description;
     public Vector3 UiPosition;
@@ -107,61 +132,82 @@ public struct DestinationData
 
     public string InfoScreenSubtitle;
     public string InfoScreenText;
-    
+
     public DestinationInfoPointData FirstInfoPoint;
     public DestinationInfoPointData SecondInfoPoint;
     public DestinationInfoPointData ThirdInfoPoint;
 }
+
+/// <summary>
+/// This data represents the text of one of the 3 info points in each destination.
+/// </summary>
 [Serializable]
 public struct DestinationInfoPointData
 {
     public string Title;
     public string Description;
 }
+
+/// <summary>
+/// This data represents the range/score and collection popup texts of a destination, once reached.
+/// </summary>
+[Serializable]
+public struct DestinationCollectableData
+{
+    public RangeCollectableData RangeData;
+
+    public PopupTextData CollectionPopup;
+}
+#endregion
+
+#region Collectable Data
+/// <summary>
+/// Represents the data of Points of intereset and actual Hayadata (DestinationCollectableData)
+/// </summary>
+[Serializable]
+public struct InfoCollectableData
+{
+    public RangeCollectableData RangeData;
+
+    public PopupTextData NotificationPopup;
+    public PopupTextData CollectionPopup;
+    public PopupTextData InfoPopup;
+}
+
 [Serializable]
 public struct InterestPointData
 {
     public string Name;
     public Vector3 WorldPosition;
-    public string Text;
+    public string InfoText;
+    public string CollectText;
 }
 
 [Serializable]
-public class CollectablePoints
+public struct BonusCollectableData
 {
-    public RangeData RangeData;
+    public RangeCollectableData RangeData;
 
-    public PopupText NotificationPopup;
-    public PopupText CollectionPopup;
+    public PopupTextData NotificationPopup;
+    public PopupTextData CollectionPopup;
 }
 
 [Serializable]
-public class StaticInterestPoints
-{
-    public Vector3 WorldPosition;
-
-    public RangeData RangeData;
-
-    public PopupText NotificationPopup;
-    public PopupText CollectionPopup;
-    public PopupText CollectedPopup;
-}
-
-[Serializable]
-public class ChallengePoints
+public struct ChallengePointData
 {
     //public float timeLimit; //gonna sit in .ini
-    public RangeData RangeData;
+    public RangeCollectableData RangeData;
 
-    public PopupText NotificationPopup;
-    public PopupText CompletedPopup;
+    public PopupTextData NotificationPopup;
+    public PopupTextData CompletedPopup;
 }
+#endregion
 
 /// <summary>
 /// Mirrors CollectableData fields
 /// </summary>
 [Serializable]
-public struct RangeData
+public struct RangeCollectableData
 {
     public float NotificationRange;
     public float CollectionRange;
@@ -172,8 +218,9 @@ public struct RangeData
 /// Mirrors PopupData fields
 /// </summary>
 [Serializable]
-public struct PopupText
+public struct PopupTextData
 {
+    public int Type;
     public string Title;
     public string Description;
     public float Duration;
