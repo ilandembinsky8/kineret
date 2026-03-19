d6081f70a1787f7ed4f951aa0d81a4718e7932cb
TomerTzipel <pporpyu@gmail.com>
2026-03-19 15:38:00 +0200

    ahhhhhhhhh

--- Kineret/Assets/Kineret/Code/Scripts/Data/GameDataManager.cs
+++ Kineret/Assets/Kineret/Code/Scripts/Data/GameDataManager.cs
@@ -124,8 +124,8 @@ public class GameDataManager : MonoBehaviour
             Debug.Log($"String: {item.Key}, Value:{item.Value != null}");
         }
 
-        LocationsManager.DestinationsCount = GameSettingsManager.GetInt("Game Settings", "DestinationGameCount  ", 3);
-        LocationsManager.DestinationsSelectCount = GameSettingsManager.GetInt("Game Settings", "DestinationsSelectCount ", 3);
+        LocationsManager.DestinationsCount = GameSettingsManager.GetInt("Game Settings", "DestinationGameCount", 3);
+        LocationsManager.DestinationsSelectCount = GameSettingsManager.GetInt("Game Settings", "DestinationsSelectCount", 3);
         LocationsManager.SelectedDestinations = new int[LocationsManager.DestinationsSelectCount];
     }
 
